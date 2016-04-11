bf9f15d11e45a210cc4f6cd1318441016e1be965
bipeo <katamason1990@gmail.com>
2016-04-11 15:41:01 +0300

    Merge origin/master
    Conflicts:
    	baltica/app/style.css

--- js/horosh.js
+++ js/horosh.js
@@ -31,8 +31,8 @@ $(function(){
     {
         var container = $(".drop.outer-sel .opts, .drop.outer-sel .select");
 
-        if (!container.is(e.target) // if the target of the click isn't the container...
-            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        if (!container.is(e.target) 
+            && container.has(e.target).length === 0) 
         {
             $('.drop.outer-sel .opts').hide();
          
