4e2bde921ed6debb88ea859100ddb18f250b32bb
bipeo <katamason1990@gmail.com>
2016-04-21 22:55:44 +0300

    Merge origin/master
    Conflicts:
    	baltica/app/style.css

--- js/chart.js
+++ js/chart.js
@@ -16,10 +16,13 @@
   }); */
      
 var chart = new Chartist.Line('.ct-chart', {
+   
   //labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,19,20],
   series: [
+      
     [3, 3, 5, 4, 3, 4, 3, 3, 3, 4, 3, 2, 3, 4, 3, 4, 3, 2, 4,5],
     [10, 9, 11, 10, 9, 11, 12, 11, 9, 10, 11, 11, 10, 9, 10, 11, 10, 9, 10, 9 ],
+    
     
   ]
 }, {
@@ -38,9 +41,34 @@ var chart = new Chartist.Line('.ct-chart', {
    showGrid: false,
    offset: 10
   },
+  
   lineSmooth: Chartist.Interpolation.cardinal({
     fillHoles: true,
   }),
 });
+
+    setTimeout(function(){
+        $('.ct-series-a .ct-point').each(function() {
+            var it = $(this).attr('ct:value');
+           
+           var value = parseInt(it) * 3.59 + 39;
+            var px = - value + 'px';
+           
+            $('.grnums.box_a').append('<li style=top:' + px + '>'+it+ '</li>');
+            
+        });
+        
+        $('.ct-series-b .ct-point').each(function() {
+            var it = $(this).attr('ct:value');
+           
+           var value = parseInt(it) * 3.66 + 64;
+            var px = - value + 'px';
+           
+            $('.grnums.box_b').append('<li style=top:' + px + '>'+it+ '</li>');
+            
+        });
+        
+    }, 0);
+   
 
  })
